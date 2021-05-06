d6be7554e0418d4024bbd0d74fe932c268a0e741
EC2 Default User <ec2-user@ip-172-31-7-215.ec2.internal>
2021-05-06 04:54:50 +0000

    完成形提出１回目

---
+++ main2.js
@@ -0,0 +1,24 @@
+        /**update(x)でACの動きを設定している**/
+        /**ACの値(value)を＝で更新・ゼロにする。ACを押した時にinputの部分に何も表示させない&オールクリアの設定をした。**/
+         function update(x){
+            document.querySelector('input').value = x
+        }
+        
+        /**appendのところはAC・＝以外のボタンを示している**/
+        /**ここでの+=の意味は数字を並べる。+を無くすと２桁以上の数字が表示されなくなってしまう。＋を入れることによって２桁以上表示させられる。**/
+        function append(x){
+            document.querySelector('input').value += x
+        }
+        
+        
+        function calc(){
+            const v = document.querySelector('input').value
+        /**try~catch文を使うことで、予期していない異常によりエラー（例外）が発生する場面で意図的にエラーを回避できる。**/
+            try{
+                const f = new Function('return ' + v)
+                update( f().toString())
+            }
+            catch(error){
+                update(error)
+                }
+            }
